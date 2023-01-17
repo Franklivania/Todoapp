d733d43c33fe028037128661a3a4edbbb3e201cc
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-01-17 22:01:38 +0100

    finished sign in form

---
+++ index.js
@@ -0,0 +1,7 @@
+function login() {
+
+    const usernameEmail = document.getElementById('username-email').value
+    const password = document.getElementById('login-password').value
+
+    alert("Invalid username or password")
+}
